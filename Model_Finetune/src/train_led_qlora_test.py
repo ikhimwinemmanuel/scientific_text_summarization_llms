3499141b82fcc76b87a1dfe75427aa1188a30569
ikhimwinemmanuel <ikhimwinemmanuel@gmail.com>
2026-05-20 11:44:35 +1000

    updated QLoRA test script to enable compatability

--- Model_Finetune/src/train_led_qlora_test.py
+++ Model_Finetune/src/train_led_qlora_test.py
@@ -152,7 +152,6 @@ def main():
         model=model,
         args=training_args,
         train_dataset=tokenized_dataset,
-        tokenizer=tokenizer,
         data_collator=data_collator,
     )
 
